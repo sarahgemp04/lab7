740c96952f4a942ab52195ff20f4f14da2d6d2b0
Sarah Gemperle <sgemperl@ucsd.edu>
2017-02-23 10:40:20 -0800

    need to deploy

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,9 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$('.likeBtn').click(likeClicked);
+}
+
+function likeClicked() {
+	ga("send", "event", 'like', 'click');
 }
